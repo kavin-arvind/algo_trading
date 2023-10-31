157022854a6e96cc8d6a58e78e2dbbb5c6e8deaf
kavin-arvind <22b1019@iitb.ac.in>
2023-11-01 00:36:02 +0530

    logic implemented, but error :(

--- phase1/trader.cpp
+++ phase1/trader.cpp
@@ -3,6 +3,12 @@
 #include <map>
 std::string notrade = "No trade";
 
+std::map <std::string, int> b_qoute; // {stock_name, value} best quote which is not cancelled
+std::map <std::string, int> s_qoute; // {stock_name, value} """
+
+std::map <std::string, int> stocks; // {stock_name, value}
+
+
 std::string tokenizer(std::string txt, char l){ // delimiter = l
     static int k=0;
     if(txt == "reset"){k=0; return "";}
@@ -29,7 +35,6 @@ int main() {
     // sleep(5);
     std::string message;    
 
-    std::map <std::string, int> stocks; // {stock_name, value}
     
     while(true){
         message = rcv.readIML();
@@ -68,7 +73,16 @@ int main() {
             i++;
             
             char mode = line[i];
-            int price = stoi(price_str);
+
+            int price;
+            try{
+                price = stoi(price_str);
+            }
+            catch (const std::invalid_argument& e){
+                std::cerr << "Invalid argument: " << price_str << std::endl;
+                price = 0;
+            }
+
             // actual working 
 
             if(stocks.find(stock_name)==stocks.end()){ // if stock encountered for the first time
@@ -87,22 +101,42 @@ int main() {
 
             else{ // if stock ALREADY encountered
                 if(mode == 's'){
-                    if(price < stocks[stock_name]){
-                        out = out + stock_name +" "+ price_str + " " + "b";
-                        stocks[stock_name] = price;
+                    if (b_qoute.find(stock_name) != b_qoute.end()){ // cancelling due to same price b and s
+                        b_qoute.erase(stock_name);
+                        out += notrade;
                     }
                     else{
-                        out += notrade;
-                    }                
+                        if(price < stocks[stock_name]){ // quote accepted
+                            out = out + stock_name +" "+ price_str + " " + "b";
+                            stocks[stock_name] = price;
+                        }
+                        else{ // quote not accepted
+                            if (s_qoute.find(stock_name) != s_qoute.end()){
+                                if (s_qoute[stock_name] > price ) {s_qoute[stock_name] = price;}
+                            }
+                            else {s_qoute[stock_name] = price;}
+                            out += notrade;
+                        }                
+                    }
                 }
-                else if(mode=='b'){
-                    if(price > stocks[stock_name]){
-                        out = out + stock_name +" "+ price_str + " " + "s";
-                        stocks[stock_name] = price;
+                else if(mode == 'b'){
+                    if (s_qoute.find(stock_name) != s_qoute.end()){ // cancelling due to same price b and s
+                        s_qoute.erase(stock_name);
+                        out += notrade;
                     }
                     else{
-                        out += notrade;
-                    }                
+                        if(price > stocks[stock_name]){ // quote accepted
+                            out = out + stock_name +" "+ price_str + " " + "s";
+                            stocks[stock_name] = price;
+                        }
+                        else{ // quote not accepted
+                            if (b_qoute.find(stock_name) != b_qoute.end()){
+                                if (b_qoute[stock_name] < price ) {b_qoute[stock_name] = price;}
+                            }
+                            else {b_qoute[stock_name] = price;}
+                            out += notrade;
+                        }                
+                    }
                 }
                 else{
                     out+="invalid request";
@@ -116,6 +150,7 @@ int main() {
             //     if(out[i]=='\0'){continue;}
             //     std::cout<<out[i];
             // }
+            std::cerr<< "printing something"<<std::endl;
             std::cout<<out<<std::endl;
 
         }
