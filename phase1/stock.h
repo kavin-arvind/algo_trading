cf2859215f83bea5d9b8a4b4c3f1999e248a4c21
kavinarvind <22b1019@iitb.ac.in>
2026-08-27 23:36:05 +0530

    Some progress but the structure is very confusing

---
+++ phase1/stock.h
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string.h>
+
+enum class TradeAction {
+    BUY = 0,
+    SELL = 1,
+    UD = 2 // Undefined
+};
+
+class Stock {
+    private:
+        std::string stock_name;
+        TradeAction tradeAction;
+        long long int price;
+        inline static int k = 0;
+        /*
+        Finds the next next tokens one by one. Reset the token if required
+        */
+    public:
+        Stock(std::string _stock_name, std::string _tradeAction, std::string _price) :
+            stock_name(_stock_name),
+            tradeAction((_tradeAction == "b" ? TradeAction::BUY : (_tradeAction == "s" ? TradeAction::SELL : TradeAction::UD))),
+            price(std::stol(_price))
+            {
+            }
+        static std::string tokeniser(std::string& txt, char l) {
+            std::string temp;
+            while (true){
+                if (txt[k]=='\0') {
+                    break;
+                }
+                if (txt[k]==l) {
+                    break;
+                }
+                temp += txt[k];
+                k++;
+            }
+            k++;
+            if(txt[k-1]=='\0'){return "";}
+            return temp;
+        }
+        static void tokeniserResetTo(int pos = 0) {
+            k = pos;
+        }
+        static int tokeniserPosition() {
+            return k;
+        }
+        friend std::ostream &operator<<(std::ostream& os, const Stock& stock) {
+            os << stock.stock_name << " ";
+            std::string temp[3] = {"Buy", "Sell", "UD"};
+            os << temp[(int)stock.tradeAction] << " ";
+            os << stock.price << " ";
+            return os;
+        }
+};
--- phase1/trader.cpp
+++ phase1/trader.cpp
@@ -1,21 +1,53 @@
 // gets data through a reciver and identifies profitable trades.
 
 #include "receiver.h"
+#include "stock.h"
 
 class Trader {
     private:
-    
+        std::string residue_message; // Unprocessed message remaining
+        void processStock(Stock& stock) {
+            std::cout << stock << "\n";
+        }
     public:
-        Trader();
+        Trader() :
+            residue_message("")
+        {
+            
+        }
+        void digestAndProcess(std::string& message) {
+            residue_message += message;
+            Stock::tokeniserResetTo(0);
+            while(Stock::tokeniserPosition() < message.size()) {
+                // Get stocks using tokeniser;
+                int starting_postion = Stock::tokeniserPosition();
+                if(Stock::tokeniser(residue_message, '#') == "") {
+                    // see this case later
+                    residue_message = std::string(residue_message.begin() + starting_postion, residue_message.end());
+                    if(residue_message == "$") {
+                        throw std::runtime_error("End Reached");
+                    }
+                    break;
+                }
+                // We've received a full line and we can process it into a full stock.
+                Stock::tokeniserResetTo(starting_postion);
+                std::string stock_name = Stock::tokeniser(residue_message, ' ');
+                std::string price = Stock::tokeniser(residue_message, ' ');
+                std::string tradeAction = Stock::tokeniser(residue_message, '#');
+                Stock currStock(stock_name, tradeAction, price);
+                processStock(currStock);
+            }
+        }
 };
 
 int main(int argc, char** argv) {
     try {
         Receiver receiver;
-        while(1) {
+        Trader trader;
+        while(true) {
             std::string message = receiver.readClient();
             if(message.size() == 0) continue;
-            std::cout << message << "\n";
+            trader.digestAndProcess(message);
         }
     } catch (const std::exception& e) {
         std::cerr << e.what() << "\n";
